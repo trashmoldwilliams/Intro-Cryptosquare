003091551b8557f7d0cb8038d115cf801295684f
lawton browning and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-19 16:22:46 -0800

    cryptosquare beta

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
-var triangle = function(sideOne, sideTwo, sideThree) {
-  if (sideOne <= (sideTwo + sideThree) && sideTwo <=(sideThree + sideOne) && sideThree <= sideOne + sideTwo) {
-    if (sideOne===sideTwo && sideTwo===sideThree) {
-      return "equilateral";
-    } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo){
-      return "isosceles";
-    } else {
-      return "scalene";
-    }
-  } else {
-    return "not a triangle";
-  }
-};
-
 $(document).ready(function() {
-  $("form#triangle").submit(function(event) {
 
-    var sideOne = parseInt($("input#sideOne").val());
-    var sideTwo = parseInt($("input#sideTwo").val());
-    var sideThree = parseInt($("input#sideThree").val());
-    var result = triangle(sideOne, sideTwo, sideThree);
-
-    sideOne = parseInt($("input#sideOne").val(""));
-    sideTwo = parseInt($("input#sideTwo").val(""));
-    sideThree = parseInt($("input#sideThree").val(""));
-
-    $("#result").text(result);
+  //Submit String
+  $("#submit-button").click(function(event) {
     event.preventDefault();
+
+    var textEntry = $("#code-entry").val().toLowerCase();
+    var entryLength = textEntry.length;
+    var alltext = "";
+
+    //Format String
+    for(var i = 0; i <= entryLength; i += 8) {
+      var line = textEntry.slice([i], [i + 9]);
+
+      //Pre-Encryption
+      $("#result").append(line);
+      $("#result").append("<br>");
+
+      //Encryption
+      alltext += line;
+    }
+
+    var alltextLength = alltext.length;
+
+    // debugger;
+    for (var i = 0; i <= 9; i++) {
+      console.log("i: " + i);
+      for (var j = 0; j <= 10 ; j += 9) {
+        console.log("j: " + j);
+        // debugger;
+        var currentCharacter = alltext.charAt(0);
+        $("#result-encryption").append(currentCharacter);
+      }
+    }
+
+    //Show String
+    // $("#result").text(textEntry);
+
   });
 });
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe("string", function() {
+  it("downcases string", function() {
+  expect(string("JOHN")).to.equal("john");
+  });
+
+})
